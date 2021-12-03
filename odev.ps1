e74d8655757102c569f4aa347094d12c662b3b4d
logistuss <50057823+logistuss@users.noreply.github.com>
2021-12-03 11:59:05 +0300

    Create odev.ps1

---
+++ odev.ps1
@@ -0,0 +1,12 @@
+﻿netstat -ano | Where-Object {$_ -match 'UDP|TCP'} | ForEach-Object {
+$veriler = $_.Trim() -split "\s+"
+[pscustomobject][ordered]@{
+    "Local Address" = $veriler[1].Split(':')[-1]
+    "ProcessId" = $veriler[4]
+    "Process Name" = $(Get-Process -Id $veriler[-1]).ProcessName 
+
+}
+}
+$veriler > C:\Users\Samet\Desktop\Yeni\test.txt
+
+
